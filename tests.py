0204281feed54d883d067af7cb3f85767a217f1e
John Eatwell <>
2025-01-26 21:13:48 +0200

    l11: reset the cell visited property

--- tests.py
+++ tests.py
@@ -35,7 +35,33 @@ class Tests(unittest.TestCase):
             m1._cells[num_cols - 1][num_rows - 1].has_bottom_wall,
             False,
         )
+    
+    def test_reset_cells_visited_all_cells_false(self):
+        # Arrange
+        from maze import Maze
+        maze = Maze(3, 3, 10, 10, 10, 10)
+        for x in range(3):
+            for y in range(3):
+                maze._cells[x][y].visited = True
+    
+        # Act
+        maze._reset_cells_visited()
+    
+        # Assert
+        for x in range(3):
+            for y in range(3):
+                assert maze._cells[x][y].visited == False
 
-
+    def test_maze_reset_cells_visited(self):
+        num_cols = 12
+        num_rows = 10
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        for col in m1._cells:
+            for cell in col:
+                self.assertEqual(
+                    cell.visited,
+                    False,
+                )
+                
 if __name__ == "__main__":
     unittest.main()
