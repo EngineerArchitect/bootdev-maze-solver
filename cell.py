17780baf75b3eeb6ca7d640422a026124eb75bc7
John Eatwell <>
2025-01-26 20:49:44 +0200

    l9: Breaking Down Walls in Maze

--- cell.py
+++ cell.py
@@ -12,10 +12,9 @@ class Cell:
         self._y2 = None
         self._win = win
         
-    def draw(self, x1:int, y1:int, x2:int, y2:int):
-        # exit gracefully if no window set
+    def draw(self, x1, y1, x2, y2):
         if self._win is None:
-            return        
+            return
         self._x1 = x1
         self._x2 = x2
         self._y1 = y1
@@ -23,15 +22,27 @@ class Cell:
         if self.has_left_wall:
             line = Line(Point(x1, y1), Point(x1, y2))
             self._win.draw_line(line)
+        else:
+            line = Line(Point(x1, y1), Point(x1, y2))
+            self._win.draw_line(line, "white")
         if self.has_top_wall:
             line = Line(Point(x1, y1), Point(x2, y1))
             self._win.draw_line(line)
+        else:
+            line = Line(Point(x1, y1), Point(x2, y1))
+            self._win.draw_line(line, "white")
         if self.has_right_wall:
             line = Line(Point(x2, y1), Point(x2, y2))
             self._win.draw_line(line)
+        else:
+            line = Line(Point(x2, y1), Point(x2, y2))
+            self._win.draw_line(line, "white")
         if self.has_bottom_wall:
             line = Line(Point(x1, y2), Point(x2, y2))
             self._win.draw_line(line)
+        else:
+            line = Line(Point(x1, y2), Point(x2, y2))
+            self._win.draw_line(line, "white")
             
     def calc_center(self):
         return Point(self._x1 + (abs(self._x2 - self._x1) // 2), 
